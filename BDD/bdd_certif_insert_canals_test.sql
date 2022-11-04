65c8ef4b2901a3ed5e768eaa1df8cf0ca050d4a2
Antoine Aribard <aaribard@gmail.com>
2022-11-04 15:30:05 +0100

    bdd add insert canals test

--- BDD/bdd_certif_insert_canals_test.sql
+++ BDD/bdd_certif_insert_canals_test.sql
@@ -1,4 +1,4 @@
 INSERT INTO canals (name, dateCreate, isRoot) VALUES
-    ('groupe 1', NOW(), 0);
-    ('groupe 1', NOW(), 0);
-    ('groupe 1', NOW(), 0);
+    ('groupe 1', NOW(), 0),
+    ('groupe 2', NOW(), 0),
+    ('groupe 3', NOW(), 0);
