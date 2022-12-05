da616e93d554b3153d54356be84e94cfb7f0854b
Antoine Aribard <aaribard@gmail.com>
2022-12-05 10:47:49 +0100

    model message

---
+++ API_Certif_Slaque/src/main/java/fr/m2i/slaque/model/Message.java
@@ -0,0 +1,65 @@
+package fr.m2i.slaque.model;
+
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "message")
+public class Message implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 2000)
+	private String corps;
+	
+	@Column(nullable = false)
+	private Date datecreation;
+	
+    @ManyToOne
+    @JoinColumn(name="idcanal", nullable=false, insertable = false, updatable = false)
+    @JsonManagedReference
+	private Canal canal;
+
+    @ManyToOne
+    @JoinColumn(name="idutilisateur", nullable=false, insertable = false, updatable = false)
+    @JsonManagedReference
+    private Utilisateur utilisateur;
+    
+	public Message() {
+		
+	}
+
+	public Message(String corps, Date datecreation, Canal canal) {
+		this.corps = corps;
+		this.datecreation = datecreation;
+		this.canal = canal;
+	}
+	
+	@Override
+	public String toString() {
+		DateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy hh:mm:ss");
+		return "[" + this.getId() + "] Canal " + this.getCanal().getId() + " - " + dateFormat.format(this.getDatecreation()) + " - " + this.getCorps();
+	}
+	
+	
+}
