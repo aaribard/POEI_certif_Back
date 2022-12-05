519bcc179621cae3201ad1805f890f406c105cc3
JeanSebastienHAMONO <jeansebastien.hamono@gmail.com>
2022-12-05 11:57:40 +0100

    Canal Service

---
+++ API_Certif_Slaque/src/main/java/fr/m2i/slaque/service/ServiceObject.java
@@ -0,0 +1,39 @@
+package fr.m2i.slaque.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public abstract class ServiceObject<T> implements ServiceInterface<T> {
+	
+	@Autowired
+	protected JpaRepository<T, Long> repository;
+
+	public List<T> getAll(){
+		return repository.findAll();
+	}
+	
+	public void save(T t) {
+		System.out.println("Save ---> ok");
+		repository.save(t);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+    public long count() {
+    	return repository.count();
+    }
+
+    public boolean existsById(Long id) {
+    	return repository.existsById(id);
+    }
+
+    public T findById(Long id) {
+    	return (T) repository.findById(id);
+    }
+}
