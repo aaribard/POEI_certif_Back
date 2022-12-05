727e2c91b91e423fe62898d09487078e9e7f6336
Antoine Aribard <aaribard@gmail.com>
2022-12-05 17:00:21 +0100

    select general query

--- API_Certif_Slaque/src/main/java/fr/m2i/slaque/repository/CanalRepository.java
+++ API_Certif_Slaque/src/main/java/fr/m2i/slaque/repository/CanalRepository.java
@@ -1,11 +1,16 @@
 package fr.m2i.slaque.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import fr.m2i.slaque.model.Canal;
 
 @Repository
 public interface CanalRepository  extends JpaRepository<Canal, Long>{
-
+	
+	@Query("SELECT c FROM Canal c WHERE isroot > 0")
+	public List<Canal> findGeneral();
 }
