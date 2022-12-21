3b2c0bf215bc5de5ddfc7ecad4f41f740d55d78a
JeanSebastienHAMONO <jeansebastien.hamono@gmail.com>
2022-12-21 11:14:32 +0100

    Pb CrossOrigine

--- API_Certif_Slaque/src/main/java/fr/m2i/slaque/controller/ControllerObject.java
+++ API_Certif_Slaque/src/main/java/fr/m2i/slaque/controller/ControllerObject.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import fr.m2i.slaque.service.ServiceInterface;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public abstract class ControllerObject<T> {
 
